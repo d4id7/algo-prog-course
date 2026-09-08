30cbf7e80e418056d7c38b7dd5568b0158302719
d4id7 <xnekto0@gmail.com>
2026-09-08 16:03:31 +0300

    Lab02: keyboard input with ReadLine

--- Lab02/obj/Debug/net10.0/Lab02.AssemblyInfo.cs
+++ Lab02/obj/Debug/net10.0/Lab02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ab923beb1fc50a0b6201b8e3c66c2da05da7929")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2505e242a97e4eb2e98f73d34f7224b474ca8b48")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
