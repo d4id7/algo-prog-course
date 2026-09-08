3251ab4e7180fa4faad0db6a37ed9edb89a515cd
d4id7 <xnekto0@gmail.com>
2026-09-08 15:05:49 +0300

    Lab01: arithmetic with variables

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -10,3 +10,41 @@ Console.WriteLine($"Группа: {groupName}");
 Console.WriteLine($"Курс: {courseNumber}");
 Console.WriteLine($"Средний балл: {averageGrade}");
 Console.WriteLine($"Бюджетное место: {isBudget}");
+
+Console.WriteLine();
+Console.WriteLine("Ремонт: комната");
+double roomWidth = 3.1415;
+double roomLength = 2.04001;
+
+double roomArea = roomWidth * roomLength;
+double roomPerimeter = (roomWidth + roomLength) * 2;
+
+Console.WriteLine($"Ширина: {roomWidth} м, длина: {roomLength} м");
+Console.WriteLine($"Площадь: {roomArea} кв.м");
+Console.WriteLine($"Ширина: {roomPerimeter} м");
+
+Console.WriteLine();
+Console.WriteLine("Покупка ноутбука в рассрочку");
+
+int laptopPrice = 65000;
+int monthsCount = 12;
+double interestRate = 0.08;
+
+double totalWithInterest = laptopPrice * (1 + interestRate);
+double monthlyPayment = totalWithInterest / monthsCount;
+
+Console.WriteLine($"Цена ноутбука: {laptopPrice} руб.");
+Console.WriteLine($"Итого в процентах: {totalWithInterest} руб.");
+Console.WriteLine($"Платёж в месяц: {monthlyPayment} руб.");
+
+Console.WriteLine();
+Console.WriteLine("Внимание: деление int");
+
+int totalStudents = 25;
+int groupsCount = 4;
+
+int studentsPerGroupWrong = totalStudents / groupsCount;
+double studentsPerGroupCorrect = (double)totalStudents / groupsCount;
+
+Console.WriteLine($"25 / 4 как int: {studentsPerGroupWrong}");
+Console.WriteLine($"25 / 4 как double: {studentsPerGroupCorrect}");
