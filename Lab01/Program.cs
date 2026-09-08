cb26bdb866938e7514bb88a8b1e94b793000cd6d
d4id7 <xnekto0@gmail.com>
2026-09-08 15:26:16 +0300

    Lab01: strings, constans, comments

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -1,4 +1,5 @@
-﻿string myName = "Илья Гордиенко";
+﻿// Сохраняет, а затем выводит данные о человеке
+string myName = "Илья Гордиенко";
 string groupName = "ИСП-251";
 int courseNumber = 2;
 double averageGrade = 4.8;
@@ -11,6 +12,7 @@ Console.WriteLine($"Курс: {courseNumber}");
 Console.WriteLine($"Средний балл: {averageGrade}");
 Console.WriteLine($"Бюджетное место: {isBudget}");
 
+// Сохраняет параметры комнаты, вычисляет и затем выводит её периметр и площадь
 Console.WriteLine();
 Console.WriteLine("Ремонт: комната");
 double roomWidth = 3.1415;
@@ -23,6 +25,7 @@ Console.WriteLine($"Ширина: {roomWidth} м, длина: {roomLength} м");
 Console.WriteLine($"Площадь: {roomArea} кв.м");
 Console.WriteLine($"Ширина: {roomPerimeter} м");
 
+// Сохраняет стоимость ноутбука и вычисляет ежемесячную платёжку, а также итоговую цену ноутбука с процентами
 Console.WriteLine();
 Console.WriteLine("Покупка ноутбука в рассрочку");
 
@@ -37,6 +40,7 @@ Console.WriteLine($"Цена ноутбука: {laptopPrice} руб.");
 Console.WriteLine($"Итого в процентах: {totalWithInterest} руб.");
 Console.WriteLine($"Платёж в месяц: {monthlyPayment} руб.");
 
+// Наглядно демонстрирует, почему при делении int на int следует перевести хотя бы одно из этих чисел в double
 Console.WriteLine();
 Console.WriteLine("Внимание: деление int");
 
@@ -48,3 +52,44 @@ double studentsPerGroupCorrect = (double)totalStudents / groupsCount;
 
 Console.WriteLine($"25 / 4 как int: {studentsPerGroupWrong}");
 Console.WriteLine($"25 / 4 как double: {studentsPerGroupCorrect}");
+
+// 3 способа сбора одной строки из нескольких 
+Console.WriteLine();
+Console.WriteLine("Способы собрать строку");
+
+string firstName = "Илья";
+string lastName = "Гордиенко";
+
+// Сп1
+string fullNameConcat = firstName + " " + lastName;
+
+// Сп2
+string fullNameInterp = $"{firstName} {lastName}";
+
+// Сп3
+string fullNameConcatMethod = string.Concat(firstName, " ", lastName);
+
+Console.WriteLine(fullNameConcat);
+Console.WriteLine(fullNameInterp);
+Console.WriteLine(fullNameConcatMethod);
+Console.WriteLine($"Все три строки равны: {fullNameConcat == fullNameInterp && fullNameInterp == fullNameConcatMethod}");
+
+// Показывает, что такое константы в C#
+Console.WriteLine();
+Console.WriteLine("Константы");
+
+const double VatRate = 0.20;
+// VatRate = 0.18;
+const string CollegeName = "ВФ ВолГУ";
+
+double productPrice = 1000;
+double priceWithVat = productPrice * (1 + VatRate);
+
+Console.WriteLine($"Учебное заведение: {CollegeName}");
+Console.WriteLine($"Цена без НСД: {productPrice}, с НСД ({VatRate}): {priceWithVat}");
+
+/*
+Многострочный
+комментарий
+*/
+
