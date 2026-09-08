d27e1623049dad92982bdd2719e9a7491f7c8787
d4id7 <xnekto0@gmail.com>
2026-09-08 16:58:43 +0300

    Lab01 final: student visitka program

--- Visitka/Program.cs
+++ Visitka/Program.cs
@@ -1,5 +1,25 @@
 ﻿// Визитка для студента
 
-string studentsName = "Гордиенко Илья Андреевич";
-string studentsGroup = "ИСП-251";
-int studentsCourse = 2;
+string studName = "Гордиенко Илья Андреевич";
+string studGroup = "ИСП-251";
+int studCourse = 2;
+string date = "08.09.2026";
+
+// Вычисляем средний балл за 3 работы
+int ftMark = 5;
+int scMark = 3;
+int tdMark = 5;
+double gpa = ((double)ftMark + scMark + tdMark) / 3;
+
+// Проверяем положена ли стипендию по среднему баллу
+bool isGrant = gpa >= 4.0;
+
+// Выводим всю информацию
+Console.WriteLine("  ВИЗИТНАЯ КАРТОЧКА СТУДЕНТА");
+Console.WriteLine($"ФИО:    {studName}");
+Console.WriteLine($"Группа: {studGroup}");
+Console.WriteLine($"Курс:   {studCourse}");
+Console.WriteLine($"Дата:   {date}");
+Console.WriteLine();
+Console.WriteLine($"Средний балл за 3 работы: {gpa}");
+Console.WriteLine($"Стипендия положена (>= 4.0): {isGrant}");
