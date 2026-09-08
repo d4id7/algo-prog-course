2505e242a97e4eb2e98f73d34f7224b474ca8b48
d4id7 <xnekto0@gmail.com>
2026-09-08 15:59:38 +0300

    Lab02: var keyword

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -39,3 +39,14 @@ decimal priceDecimal = 0.1m + 0.2m;
 
 Console.WriteLine($"double: 0.1 + 0.2 = {priceDouble}");
 Console.WriteLine($"decimal: 0.1 + 0.2 = {priceDecimal}");
+
+
+Console.WriteLine();
+Console.WriteLine("var");
+
+var studentAge = 20;    // компилятор вывел int
+// studentAge = "двадцать";
+var gpa = 4.75;     // компилятор вывел double
+var fullName = "Смирнова А.С.";     // компилятор вывел string
+
+Console.WriteLine($"{fullName}, возраст {studentAge}, средний балл {gpa}");
