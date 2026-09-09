e05df78533e08d9ff6f000d0ecdc2a073532a6e2
d4id7 <xnekto0@gmail.com>
2026-09-09 22:21:44 +0300

    Lab02 task2: name initials with char

---
+++ NameInitials/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задание 2
+Console.Write("Введите фамилию: ");
+string surnameInput = Console.ReadLine();
+
+Console.Write("Введите имя: ");
+string nameInput = Console.ReadLine();
+char ftLetterName = nameInput[0];
+
+Console.WriteLine($"{surnameInput} {ftLetterName}.");
